14e6e4a2c15c3d2e62671e8e612d494a9c434f7a
editor-5 <159111430+editor-5@users.noreply.github.com>
2025-08-31 17:43:59 +0900

    Add files via upload

---
+++ day1/main.js
@@ -0,0 +1,37 @@
+// 문제 1
+
+let a = 1
+let b = 2 
+console.log(a,b) // 1,2 
+let c = a
+a = b
+b = c 
+console.log(a,b) //  2,1 
+
+// 문제 2
+
+console.log(20 + 30)
+console.log("20" + "30")
+console.log("Hello" + " " + 2021)
+console.log(1 + 2 * 3)
+console.log((1 + 3) ** 2)
+console.log(1 / 0)
+console.log(6 % 2)
+console.log(7.5 % 2)
+console.log(5 == 5)
+console.log(5 === 5)
+console.log(5 == "5")
+console.log(5 === "5")
+console.log(5 != 5.0)
+console.log(5 !== 5.0)
+console.log("true" === true)
+console.log(5 <= 5.0)
+console.log(5 >= 5)
+console.log(true || true)
+console.log(true || false)
+console.log(true && true)
+console.log(true && false)
+console.log(!true)
+console.log(!false)
+
+ 
